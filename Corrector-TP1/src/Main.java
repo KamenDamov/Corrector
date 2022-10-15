25c6ff9176e722fdde32c148c104b59c81d174a9
KamenDamov <kamendamov123@gmail.com>
2022-10-15 00:13:28 -0400

    push

--- Corrector-TP1/src/Main.java
+++ Corrector-TP1/src/Main.java
@@ -1,6 +1,14 @@
+import javax.crypto.spec.PSource;
+
 public class Main {
     public static void main(String[] args) {
         //Declaring GUI
         gui.GUI g = new gui.GUI();
+        dictionnaire.Dico d = new dictionnaire.Dico("./dict.txt");
+        System.out.println(dictionnaire.Dico.mots("./dict.txt"));
     }
+    //TODO
+    //1) Implement levenshtein algo
+    //2) Update method
+    //3) Verify button
 }
--- Corrector-TP1/src/dictionnaire/Dico.java
+++ Corrector-TP1/src/dictionnaire/Dico.java
@@ -1,4 +1,31 @@
 package dictionnaire;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Dico {
+    String path_file;
+    public Dico(String path_file){
+        this.path_file = path_file;
+    }
+    //Dictionnaires
+    public static String mots(String path_file){
+        Path fileName
+                = Path.of(path_file);
+
+        // Now calling Files.readString() method to
+        // read the file
+        String str = null;
+        try {
+            str = Files.readString(fileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Printing the string
+        return str;
+    }
+
+
 }

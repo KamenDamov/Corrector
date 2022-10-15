66b836ab592463c061f3550caf3bb6b516b267fa
KamenDamov <kamendamov123@gmail.com>
2022-10-14 22:09:08 -0400

    push

--- Corrector-TP1/src/gui/GUI.java
+++ Corrector-TP1/src/gui/GUI.java
@@ -7,16 +7,18 @@ import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseListener;
+import java.io.File;
 import java.util.Locale;
 import java.util.Objects;
 
-public class GUI extends JFrame implements ActionListener{
+public class GUI extends JFrame {
 
     private JTextArea textArea;
     private JLabel label;
     //Declare GUI frame
     private JLabel prop;
     private JButton file;
+
     public GUI() {
         textArea = new JTextArea(30, 30);
         label = new JLabel("Corrector");
@@ -41,7 +43,23 @@ public class GUI extends JFrame implements ActionListener{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        file.addActionListener(e -> {
+            selectFile();
+        });
+
         pack();
+    }
+
+    public void selectFile() {
+        JFileChooser chooser = new JFileChooser();
+        // optionally set chooser options ...
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            File f = chooser.getSelectedFile();
+            // read  and/or display the file somehow. ....
+        } else {
+            // user changed their mind
+        }
     }
 }
 

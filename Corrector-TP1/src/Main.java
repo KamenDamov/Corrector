c88a39b924604044306ce5b513410d0fe53111aa
KamenDamov <kamendamov123@gmail.com>
2022-10-17 15:14:46 -0400

    push

--- Corrector-TP1/src/Main.java
+++ Corrector-TP1/src/Main.java
@@ -1,15 +1,25 @@
-import dictionnaire.Dico;
-
-import javax.crypto.spec.PSource;
-import java.util.ArrayList;
-
+import javax.swing.*;
+import gui.TextAreaHighlight;
 public class Main {
-    public static void main(String[] args) {
+    public static <TextAreaHighlight> void main(String[] args) {
         //Declaring GUI
-        gui.GUI g = new gui.GUI();
-        dictionnaire.Dico d = new dictionnaire.Dico("./dict.txt");
+        /*GUI g = new GUI();
+        Dico d = new Dico("./dict.txt");
         //System.out.println((ArrayList<String>) d.dicoGood());
         System.out.println(d.compute_Levenshtein_distanceDP("Kamen", "Komen"));
+        gui.TextAreaTest frame = new TextAreaTest();
+        frame.setDefaultCloseOperation( EXIT_ON_CLOSE );
+        frame.pack();
+        frame.setVisible(true);*/
+        gui.TextAreaHighlight h = new gui.TextAreaHighlight();
+        h.setTitle("TextAreaHighlight");
+        h.setVisible(true);
+        h.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        h.pack();
+        // Highlight the occurrences of the word "public"
+        h.textComp.append("Ce syst�me n'est pas vu par le public. Il est priv�.");
+        h.highlight("public");
+
     }
     //TODO
     // Add elements in grid frame

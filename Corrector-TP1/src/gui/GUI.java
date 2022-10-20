41a1227eedfbbcfa6719d94d2c3d4b20e85042de
KamenDamov <kamendamov123@gmail.com>
2022-10-19 21:43:33 -0400

    push

--- Corrector-TP1/src/gui/GUI.java
+++ Corrector-TP1/src/gui/GUI.java
@@ -146,22 +146,72 @@ public class GUI extends JFrame implements EventListener, ActionListener {
         for (int i = 0; i < texteAVerif.size(); i++) {
             boolean seen = false;
             String toCheck = texteAVerif.get(i);
-            for(int j = 0; j < texteDico.size(); j++){
-                if (toCheck != texteDico.get(i)) {
+            for (int j = 0; j < texteDico.size(); j++) {
+                if (toCheck == texteDico.get(j)) {
                     seen = true;
                 }
             }
-            if (seen = false){
+            System.out.println(seen);
+            if (seen == false) {
                 //Compute levenshtein distance as word was not found
-            }
+                for (int k = 0; k < texteDico.size(); k++) {
+                    System.out.println(compute_Levenshtein_distanceDP(toCheck, texteDico.get(k)));
+                }
 
+            }
+            System.out.println(texteDico);
+            System.out.println("Hello, I'm the check function");
+            //String s = text;
+            //List<String> l2 = new java.util.ArrayList<String>(Arrays.asList(s.split(" ")));
+            //System.out.println(dicoGood().toString());
+            //System.out.println(s);
         }
-        System.out.println(texteDico);
-        System.out.println("Hello, I'm the check function");
-        //String s = text;
-        //List<String> l2 = new java.util.ArrayList<String>(Arrays.asList(s.split(" ")));
-        //System.out.println(dicoGood().toString());
-        //System.out.println(s);
+    }
+
+    public int compute_Levenshtein_distanceDP(String str1,
+                                              String str2)
+    {
+
+        // A 2-D matrix to store previously calculated
+        // answers of subproblems in order
+        // to obtain the final
+
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for (int i = 0; i <= str1.length(); i++)
+        {
+            for (int j = 0; j <= str2.length(); j++) {
+
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+
+                else {
+                    dp[i][j] = minm_edits(dp[i - 1][j - 1]
+                                    + NumOfReplacement(str1.charAt(i - 1),str2.charAt(j - 1)), // replace
+                            dp[i - 1][j] + 1, // delete
+                            dp[i][j - 1] + 1); // insert
+                }
+            }
+        }
+
+        return dp[str1.length()][str2.length()];
+    }
+
+    static int NumOfReplacement(char c1, char c2)
+    {
+        return c1 == c2 ? 0 : 1;
+    }
+
+    static int minm_edits(int... nums)
+    {
+
+        return Arrays.stream(nums).min().orElse(
+                Integer.MAX_VALUE);
     }
 
     public void actionPerformed(ActionEvent e) {

03a51ac9a9aa824e47987fd8ff3bf0989f5f02e1
KamenDamov <kamendamov123@gmail.com>
2022-10-26 11:05:03 -0400

    push

--- Corrector-TP1/src/Main.java
+++ Corrector-TP1/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //Declaring GUI
-        gui.GUI g = new gui.GUI();
+        //gui.GUI g = new gui.GUI();
+        String s = "Alloca".substring(2,5);
+        System.out.println(s);
         //TODO
         // - Fix bugs in interface
         // - Optimize algorithm

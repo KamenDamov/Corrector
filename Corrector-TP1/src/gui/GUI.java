bdf1d090b347b1847b092d1e38089baf8d6fa972
KamenDamov <kamendamov123@gmail.com>
2022-10-26 18:24:53 -0400

    push

--- Corrector-TP1/src/gui/GUI.java
+++ Corrector-TP1/src/gui/GUI.java
@@ -30,7 +30,8 @@ public class GUI extends JFrame implements EventListener, ActionListener {
     protected JTextArea taCorrect;
     protected int startNewWord;
     protected int endNewWord;
-    String word;
+    //protected String word;
+
     Highlighter.HighlightPainter myHighlightPainter = new GUI.MyHighlightPainter(Color.red);
     static class MyHighlightPainter extends DefaultHighlighter.DefaultHighlightPainter {
         public MyHighlightPainter(Color color) {
@@ -179,13 +180,14 @@ public class GUI extends JFrame implements EventListener, ActionListener {
     }
 
     //Create a connector function - Interface to Dico
+    /*
     public void updateInterfaceToDict(String word) throws IOException {
         String toAppend = "";
         for (Object key: check().get(word).keySet()) {
             toAppend += key.toString() + "\n";
         }
         taCorrect.append(toAppend);
-    }
+    }*/
 
     //Create a connector function - Dico to interface
     public void updateDictToInterface(String word){
@@ -269,181 +271,6 @@ public class GUI extends JFrame implements EventListener, ActionListener {
     public ArrayList<String> stringArrayList(String s){
         ArrayList<String> list = new ArrayList<>(Arrays.asList(s.split(" ")));
         return list;
-    }
-    /*
-    //Structure interne de notre dico
-    public ArrayList<String> vectorize(ArrayList<String> chargerFichier, char v) {
-        if (v == 'o') {
-            ArrayList<String> newText = new ArrayList<>();
-            for (int i = 0; i < chargerFichier.size(); i++) {
-                newText.add(i, chargerFichier.get(i).toLowerCase());
-            }
-            return newText;
-        } else {
-            System.out.println("Fichier lu");
-            /*
-            StringBuilder strBuilder = new StringBuilder();
-            for (int i = 0; i < chargerFichier.size(); i++) {
-                strBuilder.append(chargerFichier.get(i));
-            }
-            String str = strBuilder.toString();
-            String clean = str.replaceAll("\\p{P}", " ").toLowerCase();
-            List<String> texte = new ArrayList<String>(Arrays.asList(clean.split(" ")));
-            //System.out.println(cleaned.toString());
-            /*
-            for (int i = 0; i < texte.size(); i++) {
-                //System.out.println(cleaned.get(i));
-                if (texte.get(i) == null || texte.get(i).isEmpty()) {
-                    texte.remove(i);
-                }
-            }
-            //System.out.println(cleaned);
-            return (ArrayList) chargerFichier;
-        }
-    }
-*/
-    //Sorting the hashmap
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
-    {
-        // Create a list from elements of HashMap
-        List<Map.Entry<String, Integer> > list =
-                new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
-
-        // Sort the list
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-
-        // put data from sorted list to hashmap
-        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
-    }
-
-    public HashMap<String, HashMap> check() throws IOException {
-        //TODO
-        // Naive algo: 2 for loops equating to O(n*m) algo
-        // For words that are not in dico
-        // Add in a hashmap KEY == DicoWord and VAL == Levenshtein Distance
-        // Sort on Val and keep only top 5
-        //Outer hashmap containing words, with its 5 closest contendants
-        HashMap<String, HashMap> wordAndDistance = new HashMap<String, HashMap>();
-        for (int i = 0; i < texteAVerif.size(); i++) {
-            String toCheck = texteAVerif.get(i);
-            boolean seen = false;
-            HashMap<String, Integer> wordLevenDistanceMap = new HashMap<String, Integer>();
-            for (int j = 0; j < texteDico.size(); j++) {
-                if (compute_Levenshtein_distanceDP(toCheck, texteDico.get(j))==0) {
-                    seen = true;
-                }
-            }
-            if (seen == false){
-                //System.out.println(toCheck);
-                for (int k = 0; k < texteDico.size(); k++) {
-                    wordLevenDistanceMap.put(texteDico.get(k), compute_Levenshtein_distanceDP(toCheck, texteDico.get(k)));
-                }
-                //System.out.println(wordAndDistance.toString());
-                Map<String, Integer> hm1 = sortByValue(wordLevenDistanceMap);
-                HashMap<String, Integer> top5Distances= new HashMap<String, Integer>();
-                int n = 0;
-                for (Map.Entry<String, Integer> en : hm1.entrySet()) {
-                    if (n == 5) {
-                        break;
-                    }
-                    top5Distances.put(en.getKey(), en.getValue());
-                    n++;
-                    wordAndDistance.put(toCheck, top5Distances);
-                }
-            }
-        }
-        for (String key : wordAndDistance.keySet()) {
-            highlight(key);
-        }
-        return wordAndDistance;
-    }
-
-    public void highlight(String pattern) {
-        // First remove all old highlights
-        removeHighlights(this.ta);
-
-        try {
-            Highlighter hilite = this.ta.getHighlighter();
-            Document doc = this.ta.getDocument();
-            String text = doc.getText(0, doc.getLength());
-            int pos = 0;
-
-            // Search for pattern
-            while ((pos = text.indexOf(pattern, pos)) >= 0) {
-                // Create highlighter using private painter and apply around pattern
-                hilite.addHighlight(pos, pos+pattern.length(), myHighlightPainter);
-                pos += pattern.length();
-            }
-        } catch (BadLocationException e) {
-        }
-    }
-
-    // Removes only our private highlights
-    public void removeHighlights(JTextComponent textComp) {
-        Highlighter hilite = textComp.getHighlighter();
-        Highlighter.Highlight[] hilites = hilite.getHighlights();
-
-        for (int i=0; i<hilites.length; i++) {
-            if (hilites[i].getPainter() instanceof TextAreaHighlight.MyHighlightPainter) {
-                hilite.removeHighlight(hilites[i]);
-            }
-        }
-    }
-
-    public int compute_Levenshtein_distanceDP(String str1,
-                                              String str2)
-    {
-
-        // A 2-D matrix to store previously calculated
-        // answers of subproblems in order
-        // to obtain the final
-
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-
-        for (int i = 0; i <= str1.length(); i++)
-        {
-            for (int j = 0; j <= str2.length(); j++) {
-
-                if (i == 0) {
-                    dp[i][j] = j;
-                }
-
-                else if (j == 0) {
-                    dp[i][j] = i;
-                }
-
-                else {
-                    dp[i][j] = minm_edits(dp[i - 1][j - 1]
-                                    + NumOfReplacement(str1.charAt(i - 1),str2.charAt(j - 1)), // replace
-                            dp[i - 1][j] + 1, // delete
-                            dp[i][j - 1] + 1); // insert
-                }
-            }
-        }
-
-        return dp[str1.length()][str2.length()];
-    }
-
-    static int NumOfReplacement(char c1, char c2)
-    {
-        return c1 == c2 ? 0 : 1;
-    }
-
-    static int minm_edits(int... nums)
-    {
-
-        return Arrays.stream(nums).min().orElse(
-                Integer.MAX_VALUE);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -547,6 +374,8 @@ public class GUI extends JFrame implements EventListener, ActionListener {
                             // Add the words instead of hello
                             // Append to textarea
                             System.out.println("The word :"+ word);
+                            System.out.println(startNewWord);
+                            System.out.println(endNewWord);
                             ta.replaceRange(word, startNewWord, endNewWord);
                             //ta.insert(word, startNewWord);
                             taCorrect.selectAll();
@@ -580,7 +409,6 @@ public class GUI extends JFrame implements EventListener, ActionListener {
                     //System.out.println( ta.getDocument().getDefaultRootElement().getElementCount() );
                 }
             });
-
         }
     }
 }
